f68065d0d95ca290acfb5365f6271caf31eee835
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-30 17:04:32 -0400

    Renammed images and added helper

---
+++ src/helpers/images.js
@@ -0,0 +1,4 @@
+export const imagesAssets = {
+  '602493b9e8f24516aab8c2455fddc44b': require('../assets/images/602493b9e8f24516aab8c2455fddc44b.jpg'),
+  '833c750ddd1f4f4da37025eecef573da': require('../assets/images/833c750ddd1f4f4da37025eecef573da.jpg'),
+};
