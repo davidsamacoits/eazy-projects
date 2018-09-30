5f2eba453902fb9068e91f4dbd9c037192274151
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-29 20:17:08 -0400

    Created ProjectCard

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,18 @@
+// ./components/ProjectCard/index.js
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import styles from './styles';
+
+const ProjectCard = () => (
+  <View>
+    <Text>
+      Hello
+    </Text>
+  </View>
+);
+
+export default ProjectCard;
