c0eb30dd0fb2156f4a13d289c9d3687363d244ac
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-30 17:08:27 -0400

    Added style for text

--- src/styles/common.js
+++ src/styles/common.js
@@ -15,3 +15,9 @@ export const SHADOW_OPACITY = 0.75;
 export const SHADOW_HEIGHT= 10;
 export const SHADOW_WIDTH = 0;
 export const SHADOW_RADIUS = 10;
+
+// TEXT
+export const TEXT_SHADOW_COLOR = 'rgba(0, 0, 0, 0.65)';
+export const TEXT_SHADOW_HEIGHT= 1;
+export const TEXT_SHADOW_WIDTH = 0;
+export const TEXT_SHADOW_RADIUS = 5;
