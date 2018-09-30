4d0aeb1f7acbbb1a110d5172cff07313a1527259
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-30 17:08:15 -0400

    Added constants

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+// ./constants.js
+
+export const STORAGE_NAME = '@eazyprojects';
+export const STORAGE_KEYS = {
+    PROJECTS: `${STORAGE_NAME}:projects`,
+};
+
+export const CURRENCIES = {
+  CAD: {
+    code: 'CAD',
+    locale: 'en-CA',
+  },
+  EUR: {
+    code: 'EUR',
+    locale: 'fr-FR',
+  },
+};
