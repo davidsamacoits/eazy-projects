733a25d84a6ac0edfa1cf1f85c3e1d42052867e4
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-30 17:19:59 -0400

    Added another image

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,4 +1,5 @@
 export const imagesAssets = {
   '602493b9e8f24516aab8c2455fddc44b': require('../assets/images/602493b9e8f24516aab8c2455fddc44b.jpg'),
   '833c750ddd1f4f4da37025eecef573da': require('../assets/images/833c750ddd1f4f4da37025eecef573da.jpg'),
+  'e2b2d5c569bf41e993a729eae0f0aab0': require('../assets/images/e2b2d5c569bf41e993a729eae0f0aab0.jpg'),
 };
