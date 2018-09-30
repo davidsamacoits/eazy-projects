022d8dd30b703b94a93cb39e3d544f3535b60c2c
David Samacoits-Etchegoin <David.SamacoitsEtchegoin@bnc.ca>
2018-09-30 17:49:57 -0400

    Added const for projects

---
+++ src/assets/projects.js
@@ -0,0 +1,10 @@
+export const projects = {
+  1: {
+    projectName: "Europe MMXIX",
+    amountSaved: "250.00",
+    imageSource: "602493b9e8f24516aab8c2455fddc44b",
+  },
+};
+
+// Hard coded project for now
+export const selectedProject = 1;
